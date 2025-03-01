6c43e396c2cde20c6380eb6e95e5bfd130a43b40
moKandji <kandji.mouhamed221@gmail.com>
2025-03-01 15:40:04 +0100

    fix

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,16 +8,86 @@ import { CommonModule } from '@angular/common';
   template: `<section id="portfolio">
     <div class="container">
       <h1 class="sub-title">Mes Projets</h1>
+      
       <div class="list-projets">
-        <div *ngFor="let projet of projets | slice:0:(afficherTout ? projets.length : 4)" class="projet">
-          <img [src]="projet.image" alt="{{ projet.title }}">
+        <!-- 4 projets visibles par défaut -->
+        <div class="projet">
+          <img src="assets/Alpanova.png" alt="Projet Alpanova">
           <div class="etape">
-            <h3>{{ projet.title }}</h3>
-            <p>{{ projet.description }}</p>
+            <h3>Projet</h3>
+            <p>Site commercial pour une station de ski</p>
+          </div>
+        </div>
+
+        <div class="projet">
+          <img src="assets/dashboardAlpanova.png" alt="Tableau de bord Alpanova">
+          <div class="etape">
+            <h3>Tableau de bord</h3>
+            <p>Gestion et prédictions des affluences de la station de ski</p>
+          </div>
+        </div>
+
+        <div class="projet">
+          <img src="assets/Sudoku.png" alt="Projet Sudoku">
+          <div class="etape">
+            <h3>Projet</h3>
+            <p>Résolution graphique de Sudoku : Backtracking, déduction & Résolveur combiné</p>
+          </div>
+        </div>
+
+        <div class="projet">
+          <img src="assets/Multidoku.png" alt="Projet Multidoku">
+          <div class="etape">
+            <h3>Projet</h3>
+            <p>Résolution graphique de Multidoku : Backtracking, déduction & Résolveur combiné</p>
           </div>
         </div>
       </div>
-      <!-- Affiche le bouton uniquement si on ne montre pas déjà tous les projets -->
+
+      <!-- Projets cachés au début, affichés avec *ngIf -->
+      <div class="list-projets hidden-projets" *ngIf="afficherTout">
+        <div class="projet">
+          <img src="assets/ExamPrepa.png" alt="Projet Académique">
+          <div class="etape">
+            <h3>Projet Académique</h3>
+            <p>Rendu final de mon projet académique en Bac+2</p>
+          </div>
+        </div>
+
+        <div class="projet">
+          <img src="assets/City.png" alt="Ville">
+          <div class="etape">
+            <h3>Ville</h3>
+            <p>Représentation d'une ville avec HTML, CSS & JavaScript</p>
+          </div>
+        </div>
+
+        <div class="projet">
+          <img src="assets/Pingouin.png" alt="Pingouin">
+          <div class="etape">
+            <h3>Pingouin</h3>
+            <p>Représentation d'un Pingouin avec HTML & CSS</p>
+          </div>
+        </div>
+
+        <div class="projet">
+          <img src="assets/SketchUp.png" alt="Maison 3D">
+          <div class="etape">
+            <h3>Maison 3D</h3>
+            <p>Modélisation d'une maison avec SketchUp & Unity</p>
+          </div>
+        </div>
+
+        <div class="projet">
+          <img src="assets/Marker.png" alt="Marqueur de couleur">
+          <div class="etape">
+            <h3>Marqueur de couleur</h3>
+            <p>Représentation du RGB</p>
+          </div>
+        </div>
+      </div>
+
+      <!-- Bouton "Et bien d'autres !" -->
       <p *ngIf="!afficherTout" class="others" (click)="afficherTout = true" style="cursor:pointer;">
         Et bien d'autres !
       </p>
@@ -26,18 +96,5 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
-  // Variable qui détermine si on affiche tous les projets ou seulement les 4 premiers
   afficherTout = false;
-
-  projets = [
-    { image: 'assets/Alpanova.png', title: 'Projet', description: 'Site commerciale pour une station de ski' },
-    { image: 'assets/dashboardAlpanova.png', title: 'Tableau de bord', description: 'Gestion et prédictions des affluences de la station de ski' },
-    { image: 'assets/Sudoku.png', title: 'Projet', description: 'Résolution graphique de Sudoku : Backtracking, déduction & Résolveur combiné' },
-    { image: 'assets/Multidoku.png', title: 'Projet', description: 'Résolution graphique de Multidoku : Backtracking, déduction & Résolveur combiné' },
-    { image: 'assets/ExamPrepa.png', title: 'Projet Académique', description: 'Rendu final de mon projet académique en Bac+2' },
-    { image: 'assets/City.png', title: 'Ville', description: 'Représentation d\'une ville avec HTML, CSS & JavaScript' },
-    { image: 'assets/Pingouin.png', title: 'Pingouin', description: 'Représentation d\'un Pingouin avec HTML & CSS' },
-    { image: 'assets/SketchUp.png', title: 'Maison 3D', description: 'Modélisation d\'une maison avec SketchUp & Unity' },
-    { image: 'assets/Marker.png', title: 'Marqueur de couleur', description: 'Représentation du RGB' }
-  ];
 }
